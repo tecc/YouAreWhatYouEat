12ff8f883e203700944e76ad5ea843721378e1d5
Chloe <56194759+Hammer86gn@users.noreply.github.com>
2022-11-05 01:37:24 -0400

    feat: LevelManager

---
+++ Assets/Scripts/Game/Util/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.Util
+{
+    public class LevelManager : UnityEngine.MonoBehaviour
+    {
+        [SerializeField]
+        List<string> levels = new List<string>();
+        
+
+        public void LoadLevel(int levelNum)
+        {
+            Math.Clamp(levelNum, 0, levels.Count - 1);
+            SceneManager.LoadScene(levels[levelNum]);
+        }
+
+        public bool LoadLevel(string name)
+        {
+            foreach (var level in levels)
+            {
+                if (level.Equals(name))
+                {
+                    SceneManager.LoadScene(level);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+    }
+}
